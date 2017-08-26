64ac5c9520441b4ed2309df3ac317618483bba2f
zhichl <e.mbryo@hotmail.com>
2017-08-26 10:42:54 -0700

    Add handleInput and move methods in Player class

--- js/app.js
+++ js/app.js
@@ -7,14 +7,21 @@ var entityInfo = {
     playerImgURL: 'images/char-boy.png',
 
     colWidth: 101,
-    rowHeight: 83
+    rowHeight: 83,
+
+    colNum: 5,
+    rowNum: 6,
+
+    canvasWidth: 505,
+    canvasHeight: 606
 
 }
 
-// This class has an update(), render()
+// Enemy class
 class Enemy {
     constructor(initX, initY, speed) {
         this.sprite = entityInfo.enemyImgURL;
+        this.initX = initX;
         this.x = initX;
         this.y = initY;
         this.speed = speed;
@@ -25,18 +32,71 @@ class Enemy {
     }
 }
 
-
+// Player class
 class Player {
     constructor(initX, initY) {
         this.character = entityInfo.playerImgURL;
         this.x = initX;
         this.y = initY;
+
+        // position by grid (5*6 grid)
+        this.gridX = entityInfo.colNum / 2;
+        this.gridY = entityInfo.rowNum - 1;
+
+        this.speedX = entityInfo.colWidth;
+        this.speedY = entityInfo.rowWidth;
     }
     update(dt) {}
     render() {
         ctx.drawImage(Resources.get(this.character), this.x, this.y);
     }
-    handleInput(direction) {}
+
+    handleInput(direction) {
+        // horizontal direction
+        if (direction === "left" || direction === "right") {
+            if (direction === "left") {
+                moveLeft();
+            } else {
+                moveRight();
+            }
+
+        // vertical direction
+        } else {
+            if (direction === "up") {
+                moveUp();
+            } else {
+                moveDown();
+            }
+        }
+    }
+
+    moveLeft() {
+        if (this.gridX !== 0) {
+            this.x -= this.speedX;
+            this.gridX -= 1;
+        }
+    }
+
+    moveRight() {
+        if (this.gridX !== entityInfo.colNum - 1) {
+            this.x += this.speedX;
+            this.gridX += 1;
+        }
+    }
+
+    moveUp() {
+        if (this.gridY !== 0) {
+            this.y -= this.speedY;
+            this.gridY -= 1;
+        }
+    }
+
+    moveDown() {
+        if (this.gridY !== entityInfo.rowWidth - 1) {
+            this.y += this.speedY;
+            this.gridY += 1;
+        }
+    }
 }
 
 
@@ -45,14 +105,17 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var initPlayer = function () {
+var player = initPlayer(),
+allEnemies = initEnemies(entityInfo.enemyCount);
+
+function initPlayer () {
     var x = entityInfo.playerInitX,
         y = entityInfo.playerInitY,
         player = new Player(x, y);
     return player;
 };
 
-var initEnemies = function (enemyCount) {
+function initEnemies (enemyCount) {
     var count,
         enemies = [],
         colW = entityInfo.colWidth,
@@ -75,11 +138,6 @@ var initEnemies = function (enemyCount) {
     // enemies.push(enemy);
     return enemies;
 };
-
-var player = initPlayer(),
-    allEnemies = initEnemies(entityInfo.enemyCount);
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
